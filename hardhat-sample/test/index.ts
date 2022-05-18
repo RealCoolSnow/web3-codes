52f0dc862aa84d66141307c0718aa4c747adb18f
CoolSnow <670696652@qq.com>
2022-05-18 10:39:20 +0800

    feat: @openzeppelin

--- hardhat-sample/test/index.ts
+++ hardhat-sample/test/index.ts
@@ -1,19 +1,34 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+// describe("Greeter", function () {
+//   it("Should return the new greeting once it's changed", async function () {
+//     const Greeter = await ethers.getContractFactory("Greeter");
+//     const greeter = await Greeter.deploy("Hello, world!");
+//     await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+//     expect(await greeter.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+//     // wait until the transaction is mined
+//     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+//     expect(await greeter.greet()).to.equal("Hola, mundo!");
+//   });
+// });
+
+describe("Learning", function () {
+  it("abc", async function () {
+    const DemoContract = await ethers.getContractFactory("MyToken");
+    const demo = await DemoContract.deploy();
+    await demo.deployed();
+
+    // const res = await demo.add();
+    // console.log(res);
+    // console.log(demo._address);
+    // await demo.sleep();
+    // const res = await demo.getBalance();
+    // console.log(res);
   });
 });
